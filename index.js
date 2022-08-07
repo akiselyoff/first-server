2486a96ba0523af69e58d0ea2e0743d29f38f85e
kesel <a.kiselyoff@gmail.com>
2022-08-07 20:19:08 +0300

    Express Server example

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express');
+const app = express();
 
 const PORT = 8081;
 const STATUS_CODE = 200;
 
-const requestHandler = async (request, response) => {
-  const manifest = await fs.readFile('./package.json', 'utf-8');
-  response.writeHead(STATUS_CODE, { 'Content-type': 'text/json' });
-  return response.end(manifest);
-};
-const server = http.createServer(requestHandler);
-server.listen(PORT, err => {
+app.get('/home', (req, res) => {
+  res.send('get request');
+});
+
+app.post('/home', (req, res) => {
+  res.send('post request');
+});
+
+app.delete('/home', (req, res) => {
+  res.send('delete request');
+});
+
+app.use((req, res) => {
+  //исп если не совпадет никакой из запросов выше. Методы выше выполняються попорядку
+  res.send('Unknown request');
+});
+
+app.listen(PORT, err => {
   if (err) {
     console.log('Error at server launch', err);
   }
