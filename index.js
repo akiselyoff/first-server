f443213b64147f915f4655f1dc5b39d7ef6c38b7
kesel <a.kiselyoff@gmail.com>
2022-08-07 18:49:48 +0300

    server request

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const PORT = 8081;
 const STATUS_CODE = 200;
 
 const requestHandler = (request, response) => {
-  response.writeHead(STATUS_CODE, { 'Content-type': 'text/html' });
+  if (request.url.indexOf('/home') >= 0) {
+    response.writeHead(STATUS_CODE, { 'Content-type': 'text/json' });
+    return response.end('{"url":"homepage"}');
+  }
+  response.writeHead(STATUS_CODE, { 'Content-type': 'text/json' });
   //   response.write('<h1>First Server</h1>');
   //   response.write('<h2>Next title</h2>');
   //   response.end(); // вызов всего, что написали в командах response.write
-  response.end('<h1>First Server</h1>');
+  response.end('{"url":"other"}');
 };
 
 const server = http.createServer(requestHandler);
