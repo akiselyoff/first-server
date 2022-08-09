747ee2ae3abaf6122ca55a01cf5ceddd5b582811
kesel <a.kiselyoff@gmail.com>
2022-08-09 19:29:09 +0300

    express middlewares

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = 8081;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //for parse form data
+app.use(express.static('public')); //for build static web site. All you want to return as site
+
 app.use((req, res, next) => {
   console.log(
     `Request method: ${req.method}, 
@@ -14,6 +18,14 @@ app.use((req, res, next) => {
 
 app.get('/home', (req, res) => {
   res.json({ javascript: 'object' });
+});
+app.post('/home', (req, res) => {
+  if (!req.body.goit) {
+    return res.status(400).json({ status: 'goit parameter is required!' });
+  }
+  console.log(req.body);
+
+  res.json({ javascript: 'object', body: req.body });
 });
 
 app.listen(PORT, err => {
